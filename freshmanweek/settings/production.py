21e8513b2fabc85475ed7375a3e34f599dda297d
Andrew Raftery <andrewraftery@gmail.com>
2014-08-06 11:49:41 -0400

    setting things up for prod

--- freshmanweek/settings/production.py
+++ freshmanweek/settings/production.py
@@ -5,7 +5,7 @@ DATABASES = {
         'ENGINE':'django.db.backends.postgresql_psycopg2',
         'NAME': 'freshmanweek',
         'USER': 'freshmanweek',
-        'PASSWORD': os.environ.get('db_password'),
+        'PASSWORD': 'freshmanweek',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
@@ -15,5 +15,5 @@ DEBUG = False
 TEMPLATE_DEBUG = False
 TIME_ZONE = 'America/New_York'
 
-STATIC_ROOT = '/var/www/static/'
+STATIC_ROOT = '/home/ubuntu/www/static/'
 STATIC_URL = '/static/'
