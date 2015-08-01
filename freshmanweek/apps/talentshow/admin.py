4b92aa55fd691bbb2d9626b73d1ff7c36bdf13f5
Andrew Raftery <andrewraftery@gmail.com>
2015-08-01 15:49:27 -0400

    send conf when auditionslot changed in backend

--- freshmanweek/apps/talentshow/admin.py
+++ freshmanweek/apps/talentshow/admin.py
@@ -1,4 +1,7 @@
+from django.conf import settings
 from django.contrib import admin
+
+from core.tasks import send_email
 
 from talentshow.models import Auditioner, AuditionSlot, AuditionSession, AuditionSignUpReminder
 
@@ -11,6 +14,27 @@ class AuditionerAdmin(admin.ModelAdmin):
 class AuditionSlotAdmin(admin.ModelAdmin):
     list_display = ('start_time', 'end_time', 'session', 'duration', 'location', 'auditioner_full_name')
     ordering = ('start_time',)
+
+    def save_model(self, request, obj, form, change):
+        if not change:
+            return super(AuditionSlotAdmin, self).save_model(request, obj, form, change)
+
+        send_conf = False
+        original_obj = AuditionSlot.objects.get(pk=obj.pk)
+
+        if obj.auditioner != original_obj.auditioner and obj.auditioner is not None:
+            send_conf = True
+
+        obj.save()
+
+        if send_conf:
+            send_email.delay(
+                template_name='audition_confirm',
+                subject='Talent Show Audition Confirmation',
+                from_email=settings.HARVARD_TALENT_EMAIL,
+                recipients=[obj.auditioner.email],
+                context={'slot': obj}
+            )
 
 
 class AuditionSignUpReminderAdmin(admin.ModelAdmin):
